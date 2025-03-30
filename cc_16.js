5bb2ec41201f011e9c6ff3c2eb27d18bc4e2358d
Abdulhakim <Abdulthe383@gmail.com>
2025-03-29 23:24:47 -0400

    Task 3: Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -6,3 +6,15 @@ fetch("https://www.course-api.com/javascript-store-products")
 console.log("Products fetched with .then():");
 products.forEach(product => console.log(product.fields.name));}) // Loged name      
 .catch(error => handleError(error));}// Handle any fetch errors  
+
+// Task 3: Fetch Products with async/await //
+async function fetchProductsAsync() {
+try {
+const response = await fetch('https://www.course-api.com/javascript-store-products');
+if (!response.ok) {
+throw new Error('Network response was not ok');}
+const data = await response.json();// Convert the response to JSON format
+displayProducts(data);}// // Display the fetched products on the page
+ catch (error) {
+ handleError(error);}} // Handling any errors
+    
